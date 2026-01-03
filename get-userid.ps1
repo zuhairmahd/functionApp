b3036250bd79f965962c8c1166b825b46c312401
Zuhair Mahmoud <zuhair@accesstojobs.com>
2026-01-02 19:59:59 -0500

    Refactor run.ps1 for improved event handling and logging; add get-userid.ps1 for user group management

---
+++ get-userid.ps1
@@ -0,0 +1,57 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [string]$UserPrincipalName,
+    [switch]$remove
+)
+
+$groupId = "817f24e3-ba30-41fb-a932-cc912fa08c73"
+$success = $false
+# Look up user by UPN and store in variable
+$user = Get-MgUser -UserId $UserPrincipalName
+$directoryObjectId = $user.Id
+if ($remove)
+{
+    # Check if user is already a member of the group
+    $isMember = Get-MgGroupMember -GroupId $groupId -All | Where-Object { $_.Id -eq $directoryObjectId }
+    if ($isMember)
+    {
+        Write-Host "Removing user $UserPrincipalName from group $groupId"
+        $success = Remove-MgGroupMemberDirectoryObjectByRef -GroupId $groupId -DirectoryObjectId $directoryObjectId -PassThru
+        if ($success)
+        {
+            Write-Host "User $UserPrincipalName removed from group $groupId successfully."
+        }
+        else
+        {
+            Write-Host "Failed to remove user $UserPrincipalName from group $groupId."
+        }
+    }
+    else
+    {
+        Write-Host "User $UserPrincipalName is not a member of group $groupId"
+    }
+}
+else
+{
+    # Check if user is already a member of the group
+    $isMember = Get-MgGroupMember -GroupId $groupId -All | Where-Object { $_.Id -eq $directoryObjectId }
+    if (-not $isMember)
+    {
+        Write-Host "Adding user $UserPrincipalName to group $groupId"
+        $success = New-MgGroupMemberByRef -GroupId $groupId -BodyParameter @{
+            "@odata.id" = "https://graph.microsoft.com/v1.0/directoryObjects/$directoryObjectId"
+        } -PassThru
+        if ($success)
+        {
+            Write-Host "User $UserPrincipalName added to group $groupId successfully."
+        }
+        else
+        {
+            Write-Host "Failed to add user $UserPrincipalName to group $groupId."
+        }
+    }
+    else
+    {
+        Write-Host "User $UserPrincipalName is already a member of group $groupId"
+    }
+}
--- requirements.psd1
+++ requirements.psd1
@@ -1,14 +1,13 @@
-# This file enables modules to be automatically managed by the Functions service.
-# See https://aka.ms/functionsmanageddependency for additional information.
-#
-@{
-    # For latest supported version, go to 'https://www.powershellgallery.com/packages/Az'.
-    # 'Az' = '15.*'
+# # This file enables modules to be automatically managed by the Functions service.
+# # See https://aka.ms/functionsmanageddependency for additional information.
+# #
+# @{
+#     # For latest supported version, go to 'https://www.powershellgallery.com/packages/Az'.
+#     'Az'                                           = '15.*'
 
-    # Microsoft Graph PowerShell SDK modules for Entra ID operations
-    # DISABLED - Modules are now manually loaded via profile.ps1 to avoid initialization conflicts
-    # 'Microsoft.Graph.Authentication'               = '2.*'
-    # 'Microsoft.Graph.Groups'                       = '2.*'
-    # 'Microsoft.Graph.Users'                        = '2.*'
-    # 'Microsoft.Graph.Identity.DirectoryManagement' = '2.*'
-}
+#     # Microsoft Graph PowerShell SDK modules for Entra ID operations
+#     'Microsoft.Graph.Authentication'               = '2.*'
+#     'Microsoft.Graph.Groups'                       = '2.*'
+#     'Microsoft.Graph.Users'                        = '2.*'
+#     'Microsoft.Graph.Identity.DirectoryManagement' = '2.*'
+# }
